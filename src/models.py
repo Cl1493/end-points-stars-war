8fba14c79d5b07814eb3ed3ccd54d40e04ca8db9
Carla <152617155+Cl1493@users.noreply.github.com>
2024-04-29 19:19:29 +0000

    models, nullable

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
     user_id = db.relationship('Favorites', backref='user_fav', nullable=False)
 
     def __repr__(self):
@@ -22,9 +22,9 @@ class User(db.Model):
     
 class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_fav.id'), nullable=False)
-    planets = db.Column(db.String(250), unique=True, nullable=False)
-    characters = db.relationship('Characters', backref='characters_fav', nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_fav.id'), nullable=True)
+    planets = db.relationship('Planets', backref='planets_fav', nullable=True)
+    characters = db.relationship('Characters', backref='characters_fav', nullable=True)
 
     def to_dict(self):
         return {}
@@ -32,25 +32,25 @@ class Favorites(db.Model):
 class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fav_id = db.Column(db.Integer, db.ForeignKey('characters_fav.id'), nullable=False)
-    name = db.Column(db.String(250), nullable=False)
-    bithyear = db.Column(db.Integer)
-    eye_color = db.Column(db.String(250))
-    hair_color = db.Column(db.String(250))
-    skin_color = db.olumn(db.String(250))
-    gender = db.Column(db.String(250))
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    bithyear = db.Column(db.Integer, nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    skin_color = db.olumn(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
 
     def to_dict(self):
         return {}
     
     class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fav_id = db.Column(db.Integer, ForeignKey('favorites.id'))
-    name = db.Column(db.String(250), nullable=False)
-    climate = db.Column(db.String(250))
-    diameter = db.Column(db.Integer)
-    terrain = db.Column(db.String(250))
-    population = db.Column(db.Integer)
-    orbital_period = db.Column(db.Integer)
+    fav_id = db.Column(db.Integer, db.ForeignKey('planets_fav.id'), nullable=False)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
     
 
     def to_dict(self):
